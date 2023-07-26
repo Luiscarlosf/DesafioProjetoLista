5bfc3d2c0b524a55f775d5df5e5cecd9649473b5
Luiscarlos02 <luiscarlos02.neto@hotmail.com>
2023-07-26 01:00:43 -0300

    add dos métodos para a funcionalidade do projeto.

--- DesafioLista/src/main/java/br/com/luiscarlosfn/DesafioLista/DesafioMercado.java
+++ DesafioLista/src/main/java/br/com/luiscarlosfn/DesafioLista/DesafioMercado.java
@@ -1,15 +1,77 @@
 package br.com.luiscarlosfn.DesafioLista;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DesafioMercado {
 
-    @GetMapping("/teste")
-    public void teste(){
+    String[] lista = new String[10];
 
-        System.out.print("Testando");
+    @GetMapping("/mercado/lista")
+    public String[] lista(){
+
+        return lista;
+
+    }
+
+    @GetMapping("/mercado/adicionar/{produto}")
+    public String adicionar(@PathVariable("produto") String produto){
+
+        String fraseDeRetorno = "";
+
+        for(int i = 0; i < lista.length; i++){
+            if(lista[i] == null){
+                lista[i] = produto;
+                fraseDeRetorno = "Produto adicionado com sucesso.";
+                break;
+            }else if(lista[i] != null){
+                fraseDeRetorno = "Não há mais espaços disponíveis";
+            }
+        }
+
+        return fraseDeRetorno;
+
+    }
+
+    @GetMapping("/mercado/deletar/{produto}")
+    public String deletar(@PathVariable("produto") String produto){
+
+        String fraseDeRetorno = "";
+
+        for(int i = 0; i < lista.length; i++){
+
+            if(lista[i].equals(produto)){
+
+                lista[i] = null;
+                fraseDeRetorno = "O item foi deletado com sucesso";
+                break;
+
+            }
+        }
+
+        return fraseDeRetorno;
+
+    }
+
+    @GetMapping("/mercado/substituir/{produtoAntigo}/{produtoNovo}")
+    public String substituir(@PathVariable("produtoAntigo") String produtoAnt, @PathVariable("produtoNovo") String produtoNov){
+
+        String fraseDeRetorno = "";
+        for(int i = 0; i < lista.length; i++){
+
+            if(lista[i].equals(produtoAnt)){
+
+                lista[i] = produtoNov;
+                fraseDeRetorno = "Produto alterado com sucesso";
+                break;
+
+            }
+
+        }
+
+        return fraseDeRetorno;
 
     }
 
